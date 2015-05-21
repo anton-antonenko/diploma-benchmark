a18e7bbde504374ea13dc117efe6b55b4cdca9a7
anton-antonenko <kingtoxa@gmail.com>
2015-05-21 17:06:18 +0300

    add tests for slow versions

--- src/main/java/anton/diploma/MyBenchmark.java
+++ src/main/java/anton/diploma/MyBenchmark.java
@@ -1,18 +1,18 @@
 /*
  * Copyright (c) 2014, Oracle America, Inc. All rights reserved.
- *
+ * 
  * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the
  * following conditions are met:
- *
+ * 
  * * Redistributions of source code must retain the above copyright notice, this list of conditions and the following
  * disclaimer.
- *
+ * 
  * * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following
  * disclaimer in the documentation and/or other materials provided with the distribution.
- *
+ * 
  * * Neither the name of Oracle nor the names of its contributors may be used to endorse or promote products derived
  * from this software without specific prior written permission.
- *
+ * 
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
  * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
  * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
@@ -38,19 +38,26 @@ import org.openjdk.jmh.runner.options.Options;
 import org.openjdk.jmh.runner.options.OptionsBuilder;
 
 import polynomial.Polynomial;
+import polynomial.PolynomialSlow;
 
 public class MyBenchmark {
 
   @State(Scope.Benchmark)
   public static class BenchmarkState {
 
-    private final BigInteger POWER = BigInteger.valueOf(17);
+    final BigInteger POWER = BigInteger.valueOf(17);
 
     Polynomial polynom1 = Polynomial.createRandom(512);
 
     Polynomial polynom2 = Polynomial.createRandom(512);
 
     Polynomial mod = Polynomial.createRandom(256);
+
+    PolynomialSlow polynomSlow1 = new PolynomialSlow(polynom1.getDegrees());
+
+    PolynomialSlow polynomSlow2 = new PolynomialSlow(polynom2.getDegrees());
+
+    PolynomialSlow modSlow = new PolynomialSlow(mod.getDegrees());
   }
 
   @Benchmark
@@ -107,16 +114,71 @@ public class MyBenchmark {
     bh.consume(poly);
   }
 
+  @Benchmark
+  public void creatingRandomPolynomialSlow(Blackhole bh) {
+    PolynomialSlow poly = PolynomialSlow.createRandom(512);
+    bh.consume(poly);
+  }
+
+  @Benchmark
+  public void addPolynomialsSlow(Blackhole bh, BenchmarkState state) {
+    PolynomialSlow poly = state.polynomSlow1.add(state.polynomSlow2);
+    bh.consume(poly);
+  }
+
+  @Benchmark
+  public void multiplyPolynomialsSlow(Blackhole bh, BenchmarkState state) {
+    PolynomialSlow poly = state.polynomSlow1.multiply(state.polynomSlow2);
+    bh.consume(poly);
+  }
+
+  @Benchmark
+  public void modPolynomialsSlow(Blackhole bh, BenchmarkState state) {
+    PolynomialSlow poly = state.polynomSlow1.mod(state.modSlow);
+    bh.consume(poly);
+  }
+
+  @Benchmark
+  public void andPolynomialsSlow(Blackhole bh, BenchmarkState state) {
+    PolynomialSlow poly = state.polynomSlow1.and(state.modSlow);
+    bh.consume(poly);
+  }
+
+  @Benchmark
+  public void orPolynomialsSlow(Blackhole bh, BenchmarkState state) {
+    PolynomialSlow poly = state.polynomSlow1.or(state.modSlow);
+    bh.consume(poly);
+  }
+
+  @Benchmark
+  public void isReduciblePolynomialSlow(Blackhole bh, BenchmarkState state) {
+    boolean reducible = state.polynomSlow1.isReducible();
+    bh.consume(reducible);
+  }
+
+  @Benchmark
+  public void gcdPolynomilasSlow(Blackhole bh, BenchmarkState state) {
+    PolynomialSlow poly = state.polynomSlow1.gcd(state.polynomSlow2);
+    bh.consume(poly);
+  }
+
+  @Benchmark
+  public void powModPolynomilasSlow(Blackhole bh, BenchmarkState state) {
+    PolynomialSlow poly = state.polynomSlow1.modPow(state.POWER, state.modSlow);
+    bh.consume(poly);
+  }
+
+  // to run benchmark: java -cp target/benchmarks.jar anton.diploma.MyBenchmark
   public static void main(String[] args) throws RunnerException {
     Options opt = new OptionsBuilder()
-      .include(MyBenchmark.class.getSimpleName())
-      .warmupIterations(10)
-      .measurementIterations(10)
-      .forks(4)
-      .mode(Mode.AverageTime)
-      .timeUnit(TimeUnit.MICROSECONDS)
-      .result("D:\\benchmarkResult.txt")
-      .build();
+    .include(MyBenchmark.class.getSimpleName())
+    .warmupIterations(10)
+    .measurementIterations(10)
+    .forks(4)
+    .mode(Mode.AverageTime)
+    .timeUnit(TimeUnit.MICROSECONDS)
+    .result("D:\\benchmarkResults.txt")
+    .build();
     new Runner(opt).run();
   }
 }
